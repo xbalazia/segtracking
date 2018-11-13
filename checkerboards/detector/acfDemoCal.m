f8af368243219e59070f133b2599686c4579a165
Michal <balazia@mail.usf.edu>
2018-11-13 15:50:54 -0500

    f

---
+++ checkerboards/detector/acfDemoCal.m
@@ -0,0 +1,95 @@
+% Demo for aggregate channel features object detector on Caltech dataset.
+%
+% (1) Download data and helper routines from Caltech Peds Website
+%  www.vision.caltech.edu/Image_Datasets/CaltechPedestrians/
+%  (1a) Download Caltech files: set*.tar and annotations.zip
+%  (1b) Copy above files to dataDir/data-USA/ and untar/unzip contents
+%  (1c) Download evaluation code (routines necessary for extracting images)
+% (2) Set dataDir/ variable below to point to location of Caltech data.
+% (3) Launch "matlabpool open" for faster training if available.
+% (4) Run demo script and enjoy your newly minted fast ped detector!
+%
+% Note: pre-trained model files are provided (delete to re-train).
+% Re-training may give slightly variable results on different machines.
+%
+% Piotr's Image&Video Toolbox      Version 3.22
+% Copyright 2013 Piotr Dollar & Ron Appel.  [pdollar-at-caltech.edu]
+% Please email me if you find bugs, or have suggestions or questions!
+% Licensed under the Simplified BSD License [see external/bsd.txt]
+
+
+%% set up environment
+clc;
+CodePath = '/BS/shanshan-projects/work/CheckerBoards_CVPR15_codebase';
+addpath(genpath(CodePath));
+versionstr = 'Checkerboards';
+
+%% set up parameters for training detector (see acfTrain_my)
+traindataDir = '/BS/shanshan-projects/work/Datasets/Caltech_Pedestrians/train_10Hz';
+testdataDir = '/BS/shanshan-projects/work/Datasets/Caltech_Pedestrians/test';
+testgtDir = '/BS/shanshan-projects/work/Datasets/Caltech_Pedestrians/test/annotations';
+
+opts=acfTrain_my();
+opts.posGtDir=[traindataDir '/annotations'];
+opts.posImgDir=[traindataDir '/images'];
+opts.name=[CodePath '/models_Caltech/' versionstr '/Checkerboards'];
+
+opts.modelDs=[96 36]; opts.modelDsPad=[120 60];
+opts.pPyramid.smooth=0; opts.pPyramid.pChns.pColor.smooth=0; 
+
+opts.pJitter=struct('flip',1);
+opts.pBoost.pTree.fracFtrs=1;
+opts.nWeak=[32 512 1024 2048 4096];
+pLoad={'lbls',{'person'},'ilbls',{'people'},'squarify',{3,.41}};
+opts.pLoad = [pLoad 'hRng',[50 inf], 'vRng',[1 1] ];
+
+opts.pPyramid.pChns.shrink = 6; opts.stride =6;opts.pPyramid.nApprox = 0;
+opts.cascThr = -1; opts.pPyramid.pChns.cbin = [2,5,5];
+opts.pPyramid.pChns.pGradHist.softBin = 1;
+opts.pPyramid.pChns.pGradHist.clipHog = Inf;
+opts.nNeg=10000;opts.nAccNeg = 50000; opts.nPerNeg = 25;
+opts.pPyramid.pChns.pGradHist.binSize=opts.pPyramid.pChns.shrink;
+opts.pPyramid.pChns.NNRadius= 1;
+opts.pPyramid.nOctUp = 1; 
+opts.pBoost.pTree.maxDepth =4;
+opts.pBoost.discrete=0;
+
+%% train detector (see acfTrain)
+detector = acfTrain_my( opts );
+
+%% modify detector (see acfModify)
+detector = acfModify_my(detector,'cascThr',-1,'cascCal',0.1);
+detector.opts.pPyramid.nPerOct = 10;
+
+save([opts.name 'Detector.mat'],'detector');
+
+sprintf('time=\t'); fix(clock)
+sprintf('\n');
+
+%% test detector and evaluate (see acfTest_my)
+vbbDir='/BS/shanshan-projects/work/Datasets/Caltech_Pedestrians/';
+tstart = tic;[miss,~,gt,dt]=acfTest_my(1, vbbDir,'name',opts.name,'imgDir',testdataDir ,...
+      'gtDir',testgtDir,'pLoad',[pLoad, 'hRng',[50 inf],...
+      'vRng',[.65 1],'xRng',[5 635],'yRng',[5 475]],'show',2);telapsed = toc(tstart);
+
+
+fid = fopen([CodePath '/models_Caltech/' versionstr '/AcfCaltechLog.txt'],'a'); 
+fprintf(fid,'\n test time=%f seconds = %f hours\n',telapsed, telapsed/3600);
+fclose(fid);
+
+sprintf('time=\t'); fix(clock)
+sprintf('\n');
+savefig([CodePath '/models_Caltech/' versionstr '/curve'],'pdf');
+close;
+
+%% run detector on a set of images without evaluation
+if(0)
+    imgNms=bbGt('getFiles',{[dataDir 'test/images']});
+    tic, bbs=acfDetect_my(imgNms,detector); toc
+    % visualize detection results on one single image
+    I=imread(imgNms{1862}); 
+    figure(1); im(I); bbApply('draw',bbs{1862}); pause(.1);
+end
+%% optionally show top false positives ('type' can be 'fp','fn','tp','dt')
+if( 0 ), bbGt('cropRes',gt,dt,imgNms,'type','fn','n',50,...
+    'show',3,'dims',opts.modelDs([2 1])); end
---
+++ checkerboards/detector/private/feature_on_templates2_autoTemplates.cpp
@@ -0,0 +1,169 @@
+/*******************************************************************************
+* Piotr's Image&Video Toolbox      Version NEW
+* Copyright 2013 Piotr Dollar.  [pdollar-at-caltech.edu]
+* Please email me if you find bugs, or have suggestions or questions!
+* Licensed under the Simplified BSD License [see external/bsd.txt]
+*******************************************************************************/
+#include "mex.h"
+#include <vector>
+#include <cmath>
+#include <string.h>
+using namespace std;
+
+typedef unsigned int uint32;
+#define MAX 16
+
+// haar struct
+typedef struct HAAR
+{ 
+    char w;
+    char h;
+    char sign[MAX];
+};
+
+
+//6*6 cells
+const short Nflt =39;
+HAAR Haar[Nflt]={
+{1,1,{1}},
+{2,2,{1,1,1,1}},
+{3,3,{1,1,1,1,1,1,1,1,1}},
+{1,2,{1,-1}},
+{1,3,{1,-1,-1}},
+{1,3,{1,1,-1}},
+{2,1,{1,-1}},
+{2,2,{1,1,-1,-1}},
+{2,2,{1,-1,1,-1}},
+{2,2,{1,-1,-1,1}},
+{2,3,{1,1,1,-1,-1,-1}},
+{2,3,{1,-1,-1,1,-1,-1}},
+{2,3,{1,1,-1,1,1,-1}},
+{2,3,{1,-1,1,-1,1,-1}},
+{3,1,{1,-1,-1}},
+{3,1,{1,1,-1}},
+{3,2,{1,1,-1,-1,-1,-1}},
+{3,2,{1,1,1,1,-1,-1}},
+{3,2,{1,-1,1,-1,1,-1}},
+{3,2,{1,-1,-1,1,1,-1}},
+{3,3,{1,1,1,-1,-1,-1,-1,-1,-1}},
+{3,3,{1,1,1,1,1,1,-1,-1,-1}},
+{3,3,{1,-1,-1,1,-1,-1,1,-1,-1}},
+{3,3,{1,1,-1,1,1,-1,1,1,-1}},
+{3,3,{1,-1,1,-1,1,-1,1,-1,1}},
+{4,1,{1,-1,-1,-1}},
+{4,1,{1,1,-1,-1}},
+{4,1,{1,1,1,-1}},
+{4,2,{1,1,-1,-1,-1,-1,-1,-1}},
+{4,2,{1,1,1,1,-1,-1,-1,-1}},
+{4,2,{1,1,1,1,1,1,-1,-1}},
+{4,2,{1,-1,1,-1,1,-1,1,-1}},
+{4,2,{1,-1,-1,1,1,-1,-1,1}},
+{4,3,{1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1}},
+{4,3,{1,1,1,1,1,1,-1,-1,-1,-1,-1,-1}},
+{4,3,{1,1,1,1,1,1,1,1,1,-1,-1,-1}},
+{4,3,{1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1}},
+{4,3,{1,1,-1,1,1,-1,1,1,-1,1,1,-1}},
+{4,3,{1,-1,1,-1,1,-1,1,-1,1,-1,1,-1}}
+  };
+
+
+  
+float* response_image(float *chns,int nChns, int width,int height,int feature_length)
+{
+    int  x1,y1, i,j,irectw,irecth, iChns,count1,count2,tag,index =0, index_res=0;
+    int sum_pixel = width*height;     
+    float *sum1 = new float[nChns];
+    float *sum2 = new float[nChns];
+    float *res = new float[feature_length];
+    int sum_size = nChns*sizeof(float);
+   // compute hist differences
+  for(int iflt = 0;iflt<Nflt;iflt++)
+  {
+        for (i =0;i<=width-Haar[iflt].w;i++)
+        {
+            for (j =0;j<=height-Haar[iflt].h;j++)
+            {
+                memset(sum1,0,sum_size);
+                memset(sum2,0,sum_size);
+                count1 =0; count2=0;
+       
+                for (irectw = 0;irectw<Haar[iflt].w;irectw++)
+                {
+                    x1 = (i+irectw);
+                    for (irecth = 0;irecth<Haar[iflt].h;irecth++)
+                    {
+                        y1 =(j+irecth);                            
+                        tag = Haar[iflt].sign[irecth*Haar[iflt].w+irectw];
+                        if (tag== 1)
+                        {
+                            count1++;
+                            for (iChns=0;iChns<nChns;iChns++)
+                            {
+                                   index =  iChns*sum_pixel + x1*height + y1;   
+                                   sum1[iChns] += chns[index];
+                                   
+                            }
+                        }
+                        else if (tag == -1)
+                        {
+                            count2++;
+                            for (iChns=0;iChns<nChns;iChns++)
+                            {
+                                   index =  iChns*sum_pixel + x1*height + y1;   
+                                    sum2[iChns] += chns[index];
+                            }
+                        }
+                    }
+                }
+                
+                
+                for (iChns=0;iChns<nChns;iChns++)
+                {
+                    if(count2>0)
+                    	res[index_res++] = (sum1[iChns]/count1- sum2[iChns]/count2);
+                    else
+                        res[index_res++] = sum1[iChns]/count1;  
+                } 
+            }
+        }
+
+  }
+    delete sum1,sum2; 
+
+    return res;
+}
+
+void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
+{
+  // get inputs
+  float *chns = (float*) mxGetData(prhs[0]);
+  const int shrink = (int) mxGetScalar(prhs[1]);
+
+  // get dimensions and constants
+  const mwSize *chnsSize = mxGetDimensions(prhs[0]);
+  const int height = (int) chnsSize[0];
+  const int width = (int) chnsSize[1];
+  const int nChns = mxGetNumberOfDimensions(prhs[0])<=2 ? 1 : (int) chnsSize[2];
+
+  // length of haar filters
+  int flen[Nflt];
+  int nhaarh[Nflt];
+  int nhaarw[Nflt];
+  int feature_length = 0;
+  for(int iflt = 0;iflt<Nflt;iflt++)
+  {
+        nhaarw[iflt] = width - Haar[iflt].w +1;
+        nhaarh[iflt] =   height-Haar[iflt].h + 1;      
+        flen[iflt] = nhaarh[iflt] * nhaarw[iflt];
+    
+        feature_length += flen[iflt];
+  }
+  feature_length *=nChns;
+  
+  plhs[0] = mxCreateNumericMatrix(1, feature_length, mxSINGLE_CLASS, mxREAL);
+  float *res = (float *) mxGetData(plhs[0]);
+  float* res1 = response_image(chns,nChns,width,height, feature_length);   
+      for (int i=0;i<feature_length;i++)
+        res[i]=res1[i];
+  delete res1;
+}
