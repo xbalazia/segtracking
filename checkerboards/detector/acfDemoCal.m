e6b97bd02ad73542c851e0f086223cf2fda7051b
Michal <balazia@mail.usf.edu>
2018-11-13 18:47:38 -0500

    lazy push balazia

--- checkerboards/detector/acfDemoCal.m
+++ checkerboards/detector/acfDemoCal.m
@@ -20,14 +20,14 @@
 
 %% set up environment
 clc;
-CodePath = '/BS/shanshan-projects/work/CheckerBoards_CVPR15_codebase';
+CodePath = '/';
 addpath(genpath(CodePath));
 versionstr = 'Checkerboards';
 
 %% set up parameters for training detector (see acfTrain_my)
-traindataDir = '/BS/shanshan-projects/work/Datasets/Caltech_Pedestrians/train_10Hz';
-testdataDir = '/BS/shanshan-projects/work/Datasets/Caltech_Pedestrians/test';
-testgtDir = '/BS/shanshan-projects/work/Datasets/Caltech_Pedestrians/test/annotations';
+traindataDir = '/datasets/Caltech_Pedestrians/train';
+testdataDir = '/datasets/Caltech_Pedestrians/test';
+testgtDir = '/datasets/Caltech_Pedestrians/test/annotations';
 
 opts=acfTrain_my();
 opts.posGtDir=[traindataDir '/annotations'];
@@ -67,7 +67,7 @@ sprintf('time=\t'); fix(clock)
 sprintf('\n');
 
 %% test detector and evaluate (see acfTest_my)
-vbbDir='/BS/shanshan-projects/work/Datasets/Caltech_Pedestrians/';
+vbbDir='/datasets/Caltech_Pedestrians/';
 tstart = tic;[miss,~,gt,dt]=acfTest_my(1, vbbDir,'name',opts.name,'imgDir',testdataDir ,...
       'gtDir',testgtDir,'pLoad',[pLoad, 'hRng',[50 inf],...
       'vRng',[.65 1],'xRng',[5 635],'yRng',[5 475]],'show',2);telapsed = toc(tstart);
