0e1d2531e528d976924ff96f2b5b5f27ce7497b2
Michal <balazia@mail.usf.edu>
2018-11-20 14:23:05 -0500

    lazy push balazia

--- external/Shu/BoW_code/do_all.m
+++
@@ -1,8 +0,0 @@
-% extract local segments
-do_representation('config_file_128_1000');
-
-% construct codebook
-do_form_codebook('config_file_128_1000');
-
-% codewords assignment
-do_vq('config_file_128_1000');
