7333238630a51f9401cf1480ed62ea447a166df9
Michal <balazia@mail.usf.edu>
2018-11-20 13:58:04 -0500

    lazy push balazia

--- checkerboards/toolbox/channels/private/gradientMex.cpp
+++ checkerboards/toolbox/channels/private/gradientMex.cpp
@@ -319,7 +319,7 @@ void fhog( float *M, float *O, float *H, int h, int w, int binSize,
 #ifdef MATLAB_MEX_FILE
 // Create [hxwxd] mxArray array, initialize to 0 if c=true
 mxArray* mxCreateMatrix3( int h, int w, int d, mxClassID id, bool c, void **I ){
-  const int dims[3]={h,w,d}, n=h*w*d; int b; mxArray* M;
+  const mwSize dims[3]={h,w,d}, n=h*w*d; int b; mxArray* M;
   if( id==mxINT32_CLASS ) b=sizeof(int);
   else if( id==mxDOUBLE_CLASS ) b=sizeof(double);
   else if( id==mxSINGLE_CLASS ) b=sizeof(float);
