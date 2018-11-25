ffa8c0a5fab43dfae02b984786a10604ca2baed6
Michal <balazia@mail.usf.edu>
2018-11-25 15:33:28 -0500

    lazy push balazia

--- faster-rcnn-2/lib/make.sh
+++ faster-rcnn-2/lib/make.sh
@@ -9,7 +9,7 @@ export CUDA_PATH=/usr/local/cuda/
 export CXXFLAGS="-std=c++11"
 export CFLAGS="-std=c99"
 
-python setup.py build_ext --inplace
+python3 setup.py build_ext --inplace
 rm -rf build
 
 CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
@@ -26,7 +26,7 @@ nvcc -c -o nms_cuda_kernel.cu.o nms_cuda_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 
 cd ../
-python build.py
+python3 build.py
 
 # compile roi_pooling
 cd ../../
@@ -35,7 +35,7 @@ echo "Compiling roi pooling kernels by nvcc..."
 nvcc -c -o roi_pooling.cu.o roi_pooling_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../
-python build.py
+python3 build.py
 
 # compile roi_align
 cd ../../
@@ -44,7 +44,7 @@ echo "Compiling roi align kernels by nvcc..."
 nvcc -c -o roi_align_kernel.cu.o roi_align_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../
-python build.py
+python3 build.py
 
 # compile roi_crop
 cd ../../
@@ -53,4 +53,4 @@ echo "Compiling roi crop kernels by nvcc..."
 nvcc -c -o roi_crop_cuda_kernel.cu.o roi_crop_cuda_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../
-python build.py
+python3 build.py
