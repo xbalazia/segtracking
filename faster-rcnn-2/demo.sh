d90c206071a39f97d00514220ead0e6536261bb1
Michal <balazia@mail.usf.edu>
2018-11-26 15:32:49 -0500

    lazy push balazia

--- faster-rcnn-2/demo.sh
+++ faster-rcnn-2/demo.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-python3 demo.py --net vgg16 --checksession $SESSION --checkepoch $EPOCH --checkpoint $CHECKPOINT --cuda --load_dir data/pretrained_model
+python3 demo.py --net vgg16 \
+                --checkpoint $CHECKPOINT \
+                --cuda --load_dir data/pretrained_model
