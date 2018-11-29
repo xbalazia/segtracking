176947a6d74729a476758babeaef13404377dc28
Michal <balazia@mail.usf.edu>
2018-11-29 16:41:35 -0500

    lazy push balazia

--- checkerboards/detector/acfDemoCal.m
+++ checkerboards/detector/acfDemoCal.m
@@ -89,8 +89,8 @@ if(1)
     imgNms=bbGt('getFiles',{[infdataDir]});
     tic, bbs=acfDetect_my(imgNms,detector); toc
     % visualize detection results on one single image
-    I=imread(imgNms{1862}); 
-    figure(1); im(I); bbApply('draw',bbs{1862}); pause(.1);
+    I=imread(imgNms{1}); 
+    figure(1); im(I); bbApply('draw',bbs{1}); pause(.1);
 end
 %% optionally show top false positives ('type' can be 'fp','fn','tp','dt')
 if(0), bbGt('cropRes',gt,dt,imgNms,'type','fn','n',50,...
