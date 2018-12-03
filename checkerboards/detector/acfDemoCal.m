3ce4e8178a749bb06b64cc664e43020dbda964a5
Michal <balazia@mail.usf.edu>
2018-12-03 17:02:59 -0500

    lazy push balazia

--- checkerboards/detector/acfDemoCal.m
+++ checkerboards/detector/acfDemoCal.m
@@ -17,30 +17,28 @@
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Simplified BSD License [see external/bsd.txt]
 
-
 %% set up environment
 clc;
 CodePath = '/home/balazia/pedtrack/checkerboards';
 addpath(genpath(CodePath));
-versionstr = 'Checkerboards';
+modelstr = 'models_Caltech';
+versionstr = 'Checkerboards-my1'; % 'Checkerboards-original' 'Checkerboards-my'
+ModelPath = [CodePath '/' modelstr '/' versionstr];
+datastr = 'data-USA';
+DataPath = [CodePath '/' datastr];
 
 %% set up parameters for training detector (see acfTrain_my)
-traindataDir = '/home/balazia/pedtrack/checkerboards/UNDEFINED';
-testdataDir = '/home/balazia/pedtrack/checkerboards/UNDEFINED';
-testgtDir = '/home/balazia/pedtrack/checkerboards/UNDEFINED';
-infdataDir = '/home/balazia/pedtrack/checkerboards/data-USA/images/set00/V000';
-
-opts=acfTrain_my();
-opts.posGtDir=[traindataDir '/annotations'];
-opts.posImgDir=[traindataDir '/images'];
-opts.name=[CodePath '/models_Caltech/' versionstr '/Checkerboards'];
+opts = acfTrain_my();
+opts.posGtDir = [DataPath '/annotations-x'];
+opts.posImgDir = [DataPath '/images-x'];
+opts.name = [ModelPath '/'];
 
 opts.modelDs=[96 36]; opts.modelDsPad=[120 60];
 opts.pPyramid.smooth=0; opts.pPyramid.pChns.pColor.smooth=0; 
 
 opts.pJitter=struct('flip',1);
 opts.pBoost.pTree.fracFtrs=1;
-opts.nWeak=[32 512 1024 2048 4096];
+opts.nWeak=[32 512]; %opts.nWeak=[32 512 1024 2048 4096];
 pLoad={'lbls',{'person'},'ilbls',{'people'},'squarify',{3,.41}};
 opts.pLoad = [pLoad 'hRng',[50 inf], 'vRng',[1 1] ];
 
@@ -51,47 +49,56 @@ opts.pPyramid.pChns.pGradHist.clipHog = Inf;
 opts.nNeg=10000;opts.nAccNeg = 50000; opts.nPerNeg = 25;
 opts.pPyramid.pChns.pGradHist.binSize=opts.pPyramid.pChns.shrink;
 opts.pPyramid.pChns.NNRadius= 1;
-opts.pPyramid.nOctUp = 1; 
+opts.pPyramid.nOctUp = 1;
 opts.pBoost.pTree.maxDepth =4;
 opts.pBoost.discrete=0;
 
 %% train detector (see acfTrain)
-detector = acfTrain_my( opts );
+if(1)
+    detector = acfTrain_my(opts);
+end
 
 %% modify detector (see acfModify)
-detector = acfModify_my(detector,'cascThr',-1,'cascCal',0.1);
-detector.opts.pPyramid.nPerOct = 10;
+if(1)
+    detector = acfModify_my(detector,'cascThr',-1,'cascCal',0.1);
+    detector.opts.pPyramid.nPerOct = 10;
 
-save([opts.name 'Detector.mat'],'detector');
+    save([opts.name 'detector.mat'],'detector');
 
-sprintf('time=\t'); fix(clock)
-sprintf('\n');
+    sprintf('time=\t'); fix(clock)
+    newline;
+end
 
 %% test detector and evaluate (see acfTest_my)
-if(0)
-    vbbDir='/home/balazia/pedtrack/checkerboards/UNDEFINED';
-    tstart = tic;[miss,~,gt,dt]=acfTest_my(1, vbbDir,'name',opts.name,'imgDir',testdataDir ,...
-      'gtDir',testgtDir,'pLoad',[pLoad, 'hRng',[50 inf],...
-      'vRng',[.65 1],'xRng',[5 635],'yRng',[5 475]],'show',2);telapsed = toc(tstart);
+if(1)
+    tstart = tic; [miss,~,gt,dt] = acfTest_my(...
+      'name', opts.name, ...
+      'imgDir', [DataPath '/images-x'] , ...
+      'gtDir', [DataPath '/annotations-x'], ...
+      'pLoad', [pLoad, 'hRng',[50 inf], 'vRng', [.65 1], 'xRng', [5 635], 'yRng',[5 475]], 'show',2);
+    telapsed = toc(tstart);
 
-
-    fid = fopen([CodePath '/models_Caltech/' versionstr '/AcfCaltechLog.txt'],'a'); 
-    fprintf(fid,'\n test time=%f seconds = %f hours\n',telapsed, telapsed/3600);
+    fid = fopen([ModelPath '/AcfCaltechLog.txt'],'a'); 
+    fprintf(fid,'\n test time = %f seconds = %f hours\n', telapsed, telapsed/3600);
     fclose(fid);
 
     sprintf('time=\t'); fix(clock)
-    sprintf('\n');
-    savefig([CodePath '/models_Caltech/' versionstr '/curve'],'pdf');
+    newline;
+    savefig([ModelPath '/curve'],'pdf');
     close;
 end
+
 %% run detector on a set of images without evaluation
-if(1)
-    imgNms=bbGt('getFiles',{[infdataDir]});
-    tic, bbs=acfDetect_my(imgNms,detector); toc
+if(0)
+    imgNms = bbGt('getFiles',{[DataPath '/images-x']});
+    tic, bbs = acfDetect_my(imgNms,detector,[ModelPath '/detections.txt']); toc
     % visualize detection results on one single image
-    I=imread(imgNms{1862}); 
-    figure(1); im(I); bbApply('draw',bbs{1862}); pause(.1);
+    %k=48;
+    %I=imread(imgNms{k});
+    %figure(1); im(I); bbApply('draw',bbs{k}); pause(.1);
 end
+
 %% optionally show top false positives ('type' can be 'fp','fn','tp','dt')
 if(0), bbGt('cropRes',gt,dt,imgNms,'type','fn','n',50,...
-    'show',3,'dims',opts.modelDs([2 1])); end
+    'show',3,'dims',opts.modelDs([2 1]));
+end
