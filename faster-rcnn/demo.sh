d816ae1c2614fd10e0ca3be89205aee0f290ea00
Michal <balazia@mail.usf.edu>
2018-12-04 13:48:39 -0500

    lazy push balazia

--- faster-rcnn/demo.sh
+++ faster-rcnn/demo.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+CUDA_VISIBLE_DEVICES=6 python3 trainval_net.py \
+                   --dataset pascal_voc --net vgg16 \
+                   --bs $BATCH_SIZE --nw $WORKER_NUMBER \
+                   --lr $LEARNING_RATE --lr_decay_step $DECAY_STEP \
+                   --cuda
+
 python3 demo.py --net vgg16 \
-                --checksession $SESSION --checkepoch $EPOCH --checkpoint $CHECKPOINT \
+                --checksession 1 --checkepoch 6 --checkpoint 10021 \
                 --cuda --load_dir data/pretrained_model
