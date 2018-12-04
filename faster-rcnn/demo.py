fab5b2cf99e7a2e9b1a7b5b3418db4b2895948c2
Michal <balazia@mail.usf.edu>
2018-12-04 17:05:57 -0500

    lazy push balazia

--- faster-rcnn/demo.py
+++ faster-rcnn/demo.py
@@ -253,7 +253,7 @@ if __name__ == '__main__':
 				im_in = np.array(frame)
 			# Load the demo image
 			else:
-				im_file = os.path.join(args.image_dir, 'test', imglist[num_images])
+				im_file = os.path.join(args.image_dir, 'test', imglist[num_images-1])
 				# im = cv2.imread(im_file)
 				im_in = np.array(imread(im_file))
 			if len(im_in.shape) == 2:
