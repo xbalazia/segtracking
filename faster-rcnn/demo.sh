7165275ed1c40758ef55bd5e300835ecada1cd0c
Michal <balazia@mail.usf.edu>
2018-12-04 14:00:42 -0500

    lazy push balazia

--- faster-rcnn/demo.sh
+++ faster-rcnn/demo.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python3 demo.py --dataset pascal_voc \
+python3 demo.py --dataset pascal_voc --image_dir data/images \
 				--load_dir data/pretrained_model --net vgg16 --cfg cfgs/vgg16.yml \
 				--checksession 1 --checkepoch 6 --checkpoint 10021
