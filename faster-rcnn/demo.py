9bf75554466d02cbe60e2387b61d13b50d1b1a4c
Michal <balazia@mail.usf.edu>
2018-12-05 17:34:50 -0500

    lazy push balazia

--- faster-rcnn/demo.py
+++ faster-rcnn/demo.py
@@ -358,7 +358,7 @@ if __name__ == '__main__':
 							if vis:
 								im2show = vis_detections(im2show, label, cls_dets.cpu().numpy(), vis_conf_threshold)
 							for bb in cls_dets:
-								print(bb[:,0])
+								print(bb)
 								bb = str(num_images)+','+str(bb).replace('tensor','').replace('(','').replace(')','').replace('[','').replace(']','').replace(' ','')
 
 								if float(bb.split(',')[-1]) >= vis_conf_threshold:
