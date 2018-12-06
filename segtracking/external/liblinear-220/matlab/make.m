8a325e5953c004989c208c718ce0a4db40707644
Michal <balazia@mail.usf.edu>
2018-12-06 18:22:56 -0500

    lazy push balazia

--- segtracking/external/liblinear-220/matlab/make.m
+++ segtracking/external/liblinear-220/matlab/make.m
@@ -3,13 +3,15 @@ function make()
 try
 	% This part is for OCTAVE
 	if(exist('OCTAVE_VERSION', 'builtin'))
+        'YES'
 		mex libsvmread.c
 		mex libsvmwrite.c
 		mex -I.. train.c linear_model_matlab.c ../linear.cpp ../tron.cpp ../blas/daxpy.c ../blas/ddot.c ../blas/dnrm2.c ../blas/dscal.c
 		mex -I.. predict.c linear_model_matlab.c ../linear.cpp ../tron.cpp ../blas/daxpy.c ../blas/ddot.c ../blas/dnrm2.c ../blas/dscal.c
 	% This part is for MATLAB
 	% Add -largeArrayDims on 64-bit machines of MATLAB
-	else
+    else
+        'NO'
 		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmread.c
 		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmwrite.c
 		mex CFLAGS="\$CFLAGS -std=c99" -I.. -largeArrayDims train.c linear_model_matlab.c ../linear.cpp ../tron.cpp ../blas/daxpy.c ../blas/ddot.c ../blas/dnrm2.c ../blas/dscal.c
