d00b4f780a3eda924745f477148a73221b6622f1
Michal <balazia@mail.usf.edu>
2018-12-07 00:30:11 -0500

    lazy push balazia

--- segtracking/external/TSP/optical_flow_celiu/mex/Coarse2FineTwoFrames.cpp
+++ segtracking/external/TSP/optical_flow_celiu/mex/Coarse2FineTwoFrames.cpp
@@ -46,7 +46,7 @@ using namespace std;
 // 	mexErrMsgTxt("Unknown type of the image!");
 // }
 
-void mexFunction(mwSize nlhs, mxArray *plhs[], mwSize nrhs, const mxArray *prhs[])
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
 	// check for proper number of input and output arguments
 	if(nrhs<2 || nrhs>3)
@@ -72,8 +72,8 @@ void mexFunction(mwSize nlhs, mxArray *plhs[], mwSize nrhs, const mxArray *prhs[
 	int nSORIterations= 20;
 	if(nrhs>2)
 	{
-		mwSize nDims=mxGetNumberOfDimensions(prhs[2]);
-		const mwSize *dims=mxGetDimensions(prhs[2]);
+		int nDims=mxGetNumberOfDimensions(prhs[2]);
+		const int *dims=mxGetDimensions(prhs[2]);
 		double* para=(double *)mxGetData(prhs[2]);
 		int npara=dims[0]*dims[1];
 		if(npara>0)
