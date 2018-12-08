51b92850969da32b9a3ee40f3ed1c7255ed8c326
Michal <balazia@mail.usf.edu>
2018-12-07 20:04:53 -0500

    lazy push balazia

--- checkerboards/detector/acfDetect_my.m
+++ checkerboards/detector/acfDetect_my.m
@@ -54,7 +54,8 @@ if( multiple ) % add image index to each bb and flatten result
   for i=1:n, bbs{i}=[ones(size(bbs{i},1),1)*i bbs{i}]; end
   bbs=cell2mat(bbs);
 end
-bbs_zeros = cat(2, zeros(size(bbs,1),1), bbs);
+bbs_zeros = cat(2, zeros(size(bbs,1),2), bbs);
+bbs_zeros = cat(2, zeros(size(bbs,2),6), bbs);
 bbs_zeros
 dlmwrite(fileName,bbs); bbs=1;
 
