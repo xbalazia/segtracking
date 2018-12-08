35ea829132fe2c2f4d236d425d139c556de8c5c1
Michal <balazia@mail.usf.edu>
2018-12-07 21:03:57 -0500

    lazy push balazia

--- faster-rcnn/demo.sh
+++ faster-rcnn/demo.sh
@@ -2,6 +2,6 @@
 
 export CUDA_VISIBLE_DEVICES=1
 
-python3 demo.py --data_dir data/ --images_dir data/images-caltech --detections_file data/detections-caltech.txt --visualizations_dir data/visualizations-caltech \
+python3 demo.py --data_dir data/ --images_dir data/images-virat --detections_file data/detections-virat.txt --visualizations_dir data/visualizations-virat \
 				--models_dir data/models --net vgg16 --cfg cfgs/vgg16.yml --checksession 1 --checkepoch 6 --checkpoint 10021 \
 				--cuda
