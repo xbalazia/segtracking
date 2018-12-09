0b987cda07aa99ec2850e4051b61ac6db01d6cd4
Michal <balazia@mail.usf.edu>
2018-12-09 14:47:33 -0500

    lazy push balazia

--- segtracking/utils/parseScene.m
+++ segtracking/utils/parseScene.m
@@ -38,14 +38,14 @@ if ~s, sceneInfo.imgFileFormat='%06d.jpg'; end
 [sceneInfo.frameNums,s]=ini.GetValues('Scene','frameNums');
 if ~s
     [~, fe]=strtok(sceneInfo.imgFileFormat,'.');    
-    imglisting=dir([sceneInfo.imgFolder, '/', sceneInfo.imgFolder, '/', '*', fe]);
+    imglisting=dir([sceneInfo.imgFolder, '/', sceneInfo.database, '/', '*', fe]);
     sceneInfo.frameNums=1:length(imglisting);
 end
 
 % image dimensions
-[sceneInfo.imgFolder '/' sceneInfo.imgFolder '/']
+[sceneInfo.imgFolder '/' sceneInfo.database '/']
 [sceneInfo.imgHeight, sceneInfo.imgWidth, ~]= ...
-    size(imread([sceneInfo.imgFolder '/' sceneInfo.imgFolder '/' sprintf(sceneInfo.imgFileFormat,sceneInfo.frameNums(1))]));
+    size(imread([sceneInfo.imgFolder '/' sceneInfo.database '/' sprintf(sceneInfo.imgFileFormat,sceneInfo.frameNums(1))]));
 
 % generic target size, will be determined based on detections
 sceneInfo.targetSize=20; 
