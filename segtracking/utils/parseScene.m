6a9e69a1d743c036bbdb1a96a8b22fa0bf55db3a
Michal <balazia@mail.usf.edu>
2018-12-09 14:37:14 -0500

    lazy push balazia

--- segtracking/utils/parseScene.m
+++ segtracking/utils/parseScene.m
@@ -14,14 +14,14 @@ end
 %assert(ini.IsKeys('Scene','frameRate'),'Need frameRate');
 assert(ini.IsKeys('Scene','imgFolder'),'Need imgFolder');
 assert(ini.IsKeys('Scene','tmpFolder'),'Need tmpFolder');
-assert(ini.IsKeys('Scene','detFile'),'Need detFile');
+assert(ini.IsKeys('Scene','detFolder'),'Need detFolder');
 assert(ini.IsKeys('Scene','trkFolder'),'Need trkFolder');
 assert(ini.IsKeys('Scene','visFolder'),'Need visFolder');
 
 %sceneInfo.frameRate = ini.GetValues('Scene','frameRate');
 sceneInfo.imgFolder = ini.GetValues('Scene','imgFolder');
 sceneInfo.tmpFolder = ini.GetValues('Scene','tmpFolder');
-sceneInfo.detFile = ini.GetValues('Scene','detFile');
+sceneInfo.detFolder = ini.GetValues('Scene','detFolder');
 sceneInfo.trkFolder = ini.GetValues('Scene','trkFolder');
 sceneInfo.visFolder = ini.GetValues('Scene','visFolder');
 
