02d1f59ea47f20478d3a398316649e65d5406254
Michal <balazia@mail.usf.edu>
2018-12-10 12:51:24 -0500

    lazy push balazia

--- segtracking/combineAllIndices.m
+++ segtracking/combineAllIndices.m
@@ -51,7 +51,7 @@ for t=1:F
         meanflowX=-flow1.flow.bvx;
         meanflowY=-flow1.flow.bvy;        
     else
-        [t length(flowinfo)] %
+        [t F length(flowinfo)] %
         flow1=flowinfo(t);
         flow2=flowinfo(t+1);
         
