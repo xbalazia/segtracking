23e84c1a22e1742e22d21301e206712ffb7960d0
Michal <balazia@mail.usf.edu>
2018-12-10 23:28:08 -0500

    lazy push balazia

--- segtracking/external/TSP/optical_flow_celiu/mex/Stochastic.h
+++ segtracking/external/TSP/optical_flow_celiu/mex/Stochastic.h
@@ -8,9 +8,9 @@
 
 #define _Release_2DArray(X,i,length) for(i=0;i<length;i++) if(X[i]!=NULL) delete X[i]; delete []X
 
-#ifndef _abs(x)
-#define _abs(x) (x>=0)?x:-x
-#endif
+//#ifndef _abs(x)
+//#define _abs(x) (x>=0)?x:-x
+//#endif
 
 //#ifndef PI
 //#define PI 3.1415927
