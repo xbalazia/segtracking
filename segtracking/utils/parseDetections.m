81cabfaa2fbdd283e53b7a970bd3921707163a15
Michal <balazia@mail.usf.edu>
2018-12-11 13:49:31 -0500

    lazy push balazia

--- segtracking/utils/parseDetections.m
+++ segtracking/utils/parseDetections.m
@@ -5,7 +5,7 @@ function [detections, nDets]=parseDetections(sceneInfo, opt, confthr)
 
 nDets=0;
 
-detFile=fullfile(sceneInfo.detFolder,sceneInfo.detector,'detections.txt');
+detFile=fullfile(sceneInfo.detFolder,'detections.txt');
 % first determine the type
 [pathstr, filename, fileext]=fileparts(detFile);
 % is there a .mat file available?
