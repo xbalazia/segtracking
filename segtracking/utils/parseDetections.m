f84d43da25ca70cdc99fa95e3ec5971f1c1cc375
Michal <balazia@mail.usf.edu>
2018-12-12 13:53:47 -0500

    lazy push balazia

--- segtracking/utils/parseDetections.m
+++ segtracking/utils/parseDetections.m
@@ -52,14 +52,14 @@ if detFileType==3
         
         w=detRaw(d,5);
         h=detRaw(d,6);
-        bx=detRaw(d,3)+w;
+        bx=detRaw(d,3);
         by=detRaw(d,4)+h;
         
         sc=detRaw(d,7);
         %sc(:)=1./(1+exp(-sc));
 
-        xi=detRaw(d,3)+w+w/2;
-        yi=detRaw(d,4)+h+h;
+        xi=detRaw(d,3)+w/2;
+        yi=detRaw(d,4)+h;
 
 	% scale to 0-1
         sc=(sc-min(detRaw(:,7))) / (max(detRaw(:,7)-min(detRaw(:,7))));
