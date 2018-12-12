4b55fc5312c8db8aa547a985f79235ce29bbf186
Michal <balazia@mail.usf.edu>
2018-12-11 20:09:34 -0500

    lazy push balazia

--- faster-rcnn/demo.sh
+++ faster-rcnn/demo.sh
@@ -4,10 +4,11 @@ export CUDA_VISIBLE_DEVICES=5
 
 DATADIR=/home/balazia/pedtrack/_data
 DATABASE=tud
+DETECTOR=faster-rcnn
 
 python3 demo.py --images_dir $DATADIR/images/$DATABASE \
-				--detections_file $DATADIR/detections/$DATABASE/detections.txt \
-				--visualizations_dir $DATADIR/detections/$DATABASE/visualizations \
+				--detections_file $DATADIR/detections/$DATABASE/$DETECTOR/detections.txt \
+				--visualizations_dir $DATADIR/detections/$DATABASE/$DETECTOR/visualizations \
 				--models_dir models --net vgg16 --cfg cfgs/vgg16.yml \
 				--checksession 1 --checkepoch 6 --checkpoint 10021 \
 				--cuda
