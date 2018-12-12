7e2bde4b32777b25b9585b6a8e73858e933e8322
Michal <balazia@mail.usf.edu>
2018-12-12 16:13:07 -0500

    lazy push balazia

--- segtracking/segTracking.m
+++ segtracking/segTracking.m
@@ -230,6 +230,7 @@ while 1
     LCost=[LCost, 0]; % background
     Scost=1-1*eye(nLabels);
 %     [Enew, D, S, L, labeling]=doAlphaExpansion(uf*Dcost, Scost, LCost, NB);
+    [Dcost Scost LCost]
     [Enew, labeling]= alphaExpansion(Dcost,Scost,LCost, labelingBeforeDO, Nhood, hypotheses, sceneInfo, opt);
     
     labeling=double(labeling);
